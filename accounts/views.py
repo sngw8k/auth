a20838c2aeacd8ab8f33b3afc50f13f9267e6966
change <och8808@gmail.com>
2023-08-28 15:06:08 +0900

    login 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login as auth_login
+
 
 # Create your views here.
 def signup(request):
@@ -17,3 +19,20 @@ def signup(request):
     }
 
     return render(request, 'signup.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:login')
+
+    else:
+        form = CustomAuthenticationForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'login.html', context)
