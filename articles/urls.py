c138bdf54ee8e131c8f10653565374065dd7299f
change <och8808@gmail.com>
2023-08-29 14:04:58 +0900

    Comment Create 기능 구현

--- articles/urls.py
+++ articles/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     path('create/', views.create, name='create'),
+
+    path('<int:article_id>/comments/create/', views.comment_create, name='comment_create'),
     
 ]
